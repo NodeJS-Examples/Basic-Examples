02ce05a029033875078ba934f1e7822cbe303ce7
ErikOverflow <ErikOverflow@gmail.com>
2020-02-19 20:27:06 +0000

    Add dynamic file server example

---
+++ URL/dynamicfileserver.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  //parse the url into a URL object
+  var q = url.parse(req.url, true);
+  //Grab the path from the URL
+  var filename = "." + q.pathname;
+  //Read the file in the path (from the directory local to this server)
+  fs.readFile(filename, function(err, data) {
+    //If file is not there, return 404
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    
+    //If file is there, return the data as html text.
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
