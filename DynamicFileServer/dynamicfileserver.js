6cf8d1475de5d110fa8c1506f9ac848b1bad2bd9
ErikOverflow <ErikOverflow@gmail.com>
2020-02-19 20:38:51 +0000

    Comment and clean up dynamic file server example

--- DynamicFileServer/dynamicfileserver.js
+++ DynamicFileServer/dynamicfileserver.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   //Grab the path from the URL
   var filename = "." + q.pathname;
-  //Read the file in the path (from the directory local to this server)
+  //Read the file in the path (from the directory local to this server). The only file in this project is "Test.html"
   fs.readFile(filename, function(err, data) {
     //If file is not there, return 404
     if (err) {
@@ -15,7 +15,7 @@ http.createServer(function (req, res) {
       return res.end("404 Not Found");
     } 
     
-    //If file is there, return the data as html text.
+    //If file is there, write the file's data content to the response
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
