727cd1d346f4e61f78a473c44ae66dcc0494c026
ErikOverflow <ErikOverflow@gmail.com>
2020-02-20 17:07:15 +0000

    Set up inserting to MongoDB

--- MongoDB Insert/dogoperations.js
+++ MongoDB Insert/dogoperations.js
@@ -1,12 +1,24 @@
-exports.InsertDog = function(dbObj, color, breed, weight) {
-    var dogObj =
-    {
-        color: color,
-        breed: breed,
-        weight: weight
+module.exports = function(dbObj){
+    var module = {};
+    
+    var dogCollection = dbObj.collection("dogs");
+    
+    module.InsertDog = function(color, breed, weight) {
+        var dogObj =
+        {
+            color: color,
+            breed: breed,
+            weight: weight
+        };
+        dogCollection.insertOne(dogObj, function(err, res) {
+            if (err) throw err;
+            console.log("Dog inserted");
+            });
     };
-  dbObj.collection("dogs").insertOne(dogObj, function(err, res) {
-    if (err) throw err;
-    console.log("Dog inserted");
-  });
+    
+    module.LogAnnouncement = function() {
+        console.log("Successful");
+    };
+    
+    return module;
 };
--- MongoDB Insert/server.js
+++ MongoDB Insert/server.js
@@ -1,12 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-var dogOps = require('./dogoperations');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("animalsdb");
-  
-  dogOps.InsertDog(dbo, "Brown", "Chihuahua", 12);
-  
-  db.close();
+
+MongoClient.connect(url, { useUnifiedTopology: true, poolSize: 10 }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("animalsdb");
+    
+    //Create a dogOperations var here
+    var dogOps = require('./dogoperations')(dbo);
+    dogOps.InsertDog("White", "Schnauzer", 50);
+    //We do not close the DB, since that would kill the connection before the asynchronous insertOne happens.
 });
